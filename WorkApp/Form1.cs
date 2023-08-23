88439a2b6adb9d39b14db3072623b59c646ff56e
Yavuz Selim KAHRAMAN <yskahraman@gmail.com>
2023-08-23 23:00:18 +0300

    work completed

--- WorkApp/Form1.cs
+++ WorkApp/Form1.cs
@@ -1,3 +1,5 @@
+using Microsoft.Data.Sqlite;
+
 namespace WorkApp
 {
     public partial class Form1 : Form
@@ -5,6 +7,20 @@ namespace WorkApp
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(!string.IsNullOrEmpty(textBox1.Text) && !string.IsNullOrEmpty(textBox2.Text))
+            {
+                // pathe nasýl ulaþabilirsiniz ? 
+                // 
+                SqliteConnection connection = new SqliteConnection("");
+                SqliteCommand command = new SqliteCommand();
+                command.CommandText = "select * from Users where username =@username";
+
+                
+            }
         }
     }
 }
