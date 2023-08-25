f4af2c5d1dea9530ba4d2b2a22537cc0d1abe2a9
Murat ACAR <muratacar@valfsel.com.tr>
2023-08-25 15:47:23 +0300

    db-path-manage

--- WorkApp/Form1.cs
+++ WorkApp/Form1.cs
@@ -1,4 +1,4 @@
-using Microsoft.Data.Sqlite;
+using System.Data.SQLite;
 
 namespace WorkApp
 {
@@ -11,15 +11,34 @@ namespace WorkApp
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if(!string.IsNullOrEmpty(textBox1.Text) && !string.IsNullOrEmpty(textBox2.Text))
+            string projectDirectory = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\.."));
+            DirectoryInfo directoryInfo = new DirectoryInfo(projectDirectory);
+            var folders = directoryInfo.GetDirectories();
+            var dbDirectory = folders.FirstOrDefault(p => p.Name == "Db");
+
+            if (!string.IsNullOrEmpty(textBox1.Text) && !string.IsNullOrEmpty(textBox2.Text) && dbDirectory != null)
             {
                 // pathe nasýl ulaþabilirsiniz ? 
                 // 
-                SqliteConnection connection = new SqliteConnection("");
-                SqliteCommand command = new SqliteCommand();
-                command.CommandText = "select * from Users where username =@username";
+                MessageBox.Show("Db dosya yolu : " + dbDirectory.FullName);
+                var dbFiles = dbDirectory.GetFiles();
+                var db = dbFiles.FirstOrDefault(p => p.Extension == ".db");
 
-                
+                SQLiteConnection connection = new SQLiteConnection(@"Data Source=" + db);
+                SQLiteCommand command = connection.CreateCommand();
+                SQLiteDataReader dataReader;
+                connection.Open();
+                command.CommandText = "SELECT * FROM Person";
+                dataReader = command.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    MessageBox.Show(dataReader.GetInt32(0).ToString()+ " " + dataReader.GetString(1).ToString());
+                }
+
+                connection.Close();
+
+
             }
         }
     }
